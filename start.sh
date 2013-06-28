b4bf3ad9c446f1822df0aa18bcd9c575314ab8e3
Jean Parpaillon <jean.parpaillon@free.fr>
2013-06-28 12:44:29 +0200

    Format DFS and start Hadoop services

--- start.sh
+++ start.sh
@@ -9,19 +9,24 @@ if test ! -d $hadoopdir; then
     echo "No Hadoop. Please run setup.mk"
 fi
 
+masternode=$(head -1 nodes.out)
+cfdir=$hadoopdir/conf
+dfsdir=/tmp/hdfs
+jobid=$(cat jobid.out)
+
+###
+echo "Clean up"
+rm -fr $dfsdir
+
 ###
 echo "Starting Hadoop session"
 ###
 llsubmit ssh.cmd 2>&1 | grep "The job" | sed -e 's/^llsubmit: The job "\([a-z0-9\.]\+\)\".*/\1/' > jobid.out
-jobid=$(cat jobid.out)
 llq -l $jobid | grep 'Task Instance' | tail -4 | sed -e 's/^.*\(node[0-9]\+\).*/\1/' > nodes.out
 
 ###
 echo "Setting up hadoop site"
 ###
-masternode=$(head -1 nodes.out)
-cfdir=$hadoopdir/conf
-dfsdir=/tmp/hdfs
 sed -e 's/@MASTERNODE@/'$masternode'/g' conf/core-site.xml > $cfdir/core-site.xml
 sed -e 's/@DFSDIR@/'$dfsdir'/g' conf/hdfs-site.xml > $cfdir/hdfs-site.xml
 sed -e 's/@MASTERNODE@/'$masternode'/g; s/@HADOOPDIR@/'$hadoopdir'/g' conf/mapred-site.xml > $cfdir/mapred-site.xml
@@ -30,6 +35,7 @@ tail -n +2 nodes.out > $cfdir/slaves
 ###
 echo "Starting Hadoop cluster from $masternode"
 ###
+ssh $masternode $hadoopdir/bin/hadoop namenode -format
 ssh $masternode $hadoopdir/bin/start-all.sh
 
 exit 0
--- stop.sh
+++ stop.sh
@@ -7,12 +7,31 @@ nojobid() {
     exit 0
 }
 
+topdir=$(dirname $0)
+hadoopdir=$topdir/hadoop-1.2.0
+
 if test ! -e jobid.out; then
     nojobid
 fi
 
+if test ! -d $hadoopdir; then
+    echo "No Hadoop. Please run setup.mk"
+fi
+
+masternode=$(head -1 nodes.out)
+cfdir=$hadoopdir/conf
+dfsdir=/tmp/hdfs
 jobid=$(cat jobid.out)
+
+###
+echo "Stopping Hadoop services"
+###
+ssh $masternode $hadoopdir/bin/stop-all.sh
+ssh $masternode "rm -rf $dfsdir"
+
+###
 echo "Stopping Hadoop session $jobid"
+###
 llcancel $jobid
 
 rm -f jobid.out
